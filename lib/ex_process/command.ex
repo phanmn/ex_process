310321a924dca5079c94898535aab0c395c12669
Nhut Phan <thanquan@gmail.com>
2024-04-03 00:01:56 +0700

    feat: Add try_wait

--- lib/ex_process/command.ex
+++ lib/ex_process/command.ex
@@ -3,4 +3,5 @@ defmodule ExProcess.Command do
 
   # When your NIF is loaded, it will override this function.
   def spawn(_command, _args, _envs), do: :erlang.nif_error(:nif_not_loaded)
+  def try_wait(_resource), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/command/src/lib.rs
+++ native/command/src/lib.rs
@@ -6,7 +6,7 @@ struct ProcessResource {
     pub child: Mutex<std::process::Child>,
 }
 
-rustler::atoms! { error, ok, }
+rustler::atoms! { error, ok, none}
 
 enum SpawnResult {
     Success(ResourceArc<ProcessResource>),
@@ -22,12 +22,7 @@ impl<'a> Encoder for SpawnResult {
     }
 }
 
-fn load(env: Env, _: Term) -> bool {
-    rustler::resource!(ProcessResource, env);
-    true
-}
-
-#[rustler::nif(schedule = "DirtyCpu")]
+#[rustler::nif]
 fn spawn(
     program: String,
     arg_list: Vec<String>,
@@ -46,4 +41,35 @@ fn spawn(
     }
 }
 
-rustler::init!("Elixir.ExProcess.Command", [spawn], load = load);
+enum TryWaitResult {
+    Success(i32),
+    None,
+    Failure(String),
+}
+
+impl<'a> Encoder for TryWaitResult {
+    fn encode<'b>(&self, env: Env<'b>) -> Term<'b> {
+        match self {
+            TryWaitResult::Success(arc) => (ok(), arc).encode(env),
+            TryWaitResult::Failure(msg) => (error(), msg).encode(env),
+            TryWaitResult::None => (ok(), none()).encode(env),
+        }
+    }
+}
+
+fn load(env: Env, _: Term) -> bool {
+    rustler::resource!(ProcessResource, env);
+    true
+}
+
+#[rustler::nif]
+fn try_wait(resource: ResourceArc<ProcessResource>) -> TryWaitResult {
+    let child = &mut *resource.child.lock().unwrap();
+    match child.try_wait() {
+        Ok(Some(status)) => TryWaitResult::Success(status.code().unwrap()),
+        Ok(None) => TryWaitResult::None,
+        Err(e) => TryWaitResult::Failure(format!("{:#}", e)), // Err(format!("{:#}", e)),
+    }
+}
+
+rustler::init!("Elixir.ExProcess.Command", [spawn, try_wait], load = load);
