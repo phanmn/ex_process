7da24fc8314e060b9a66c516fb646424445b7cc3
Nhut Phan <thanquan@gmail.com>
2024-04-03 08:59:36 +0700

    update: CommandTest

--- test/ex_process/command_test.exs
+++ test/ex_process/command_test.exs
@@ -2,9 +2,7 @@ defmodule ExProcess.CommandTest do
   use ExUnit.Case
   require Logger
 
-  test "greets the world" do
-    ExProcess.Command.run("123", ["456", "789"], %{"abc" => "xyz"})
-    |> inspect()
-    |> Logger.error()
+  test "invalid" do
+    assert {:error, _} = ExProcess.Command.spawn("123", ["456", "789"], %{"abc" => "xyz"})
   end
 end
