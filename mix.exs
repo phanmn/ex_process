2cb3de25728e2741af46140bd283ef2faa376e64
Nhut Phan <thanquan@gmail.com>
2024-04-09 09:13:54 +0700

    update: set version

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule ExProcess.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :ex_process,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
