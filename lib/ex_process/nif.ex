f4ee09a66a8d15ea2fe86de846a086eafebe8dbb
Nhut Phan <thanquan@gmail.com>
2024-04-09 19:31:05 +0700

    feat: Add Nif.rustler_precompile_config

--- lib/ex_process/nif.ex
+++ lib/ex_process/nif.ex
@@ -3,13 +3,20 @@ defmodule ExProcess.Nif do
 
   version = Mix.Project.config()[:version]
 
-  use RustlerPrecompiled,
-    otp_app: :ex_process,
-    crate: "command",
-    base_url:
-      "https://github.com/phanmn/ex_process/releases/download/v#{version}",
-    force_build: System.get_env("EX_PROCESS_BUILD") in ["1", "true"],
-    version: version
+  @rustler_precompiled otp_app: :ex_process,
+                       crate: "command",
+                       base_url:
+                         "https://github.com/phanmn/ex_process/releases/download/v#{version}",
+                       force_build: System.get_env("EX_PROCESS_BUILD") in ["1", "true"],
+                       version: version
+
+  use RustlerPrecompiled, @rustler_precompiled
+
+  def rustler_precompile_config() do
+    @rustler_precompiled
+    |> Keyword.put_new(:module, __MODULE__)
+    |> RustlerPrecompiled.Config.new()
+  end
 
   # When your NIF is loaded, it will override this function.
   def spawn(_runtime, _command, _args, _envs), do: :erlang.nif_error(:nif_not_loaded)
