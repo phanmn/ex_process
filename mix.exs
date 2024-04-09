69f9494dc0dc194966e1620e1d75c28c9aea8d06
Nhut Phan <thanquan@gmail.com>
2024-04-09 08:54:14 +0700

    update: add using RustlerPrecompiled

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExProcess.MixProject do
     [
       app: :ex_process,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,7 +21,8 @@ defmodule ExProcess.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.32.1", runtime: false}
+      {:rustler, "~> 0.32.1", runtime: false},
+      {:rustler_precompiled, "~> 0.7", runtime: false}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
